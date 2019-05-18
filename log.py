a9e74c1c7ca25401485c2fe2ab2e03dc58a46ee6
Ramiro Estrada <ramiroestradag@gmail.com>
2019-05-17 20:59:40 -0500

    Update log.py

--- log.py
+++ log.py
@@ -1,16 +1,16 @@
-from sty import fg
 import sys
 
 
 class Log:
     @staticmethod
     def e(tag, msg):
-        sys.stdout.write(fg.red+"ERROR/"+str(tag)+"\t\t"+str(msg)+"\n")
+        sys.stdout.write("ERROR/"+str(tag)+"\t\t"+str(msg)+"\n")
 
     @staticmethod
     def i(tag, msg):
-        sys.stdout.write(fg.blue+"INFO /"+str(tag)+"\t\t"+str(msg)+"\n")
+        sys.stdout.write("INFO /"+str(tag)+"\t\t"+str(msg)+"\n")
 
     @staticmethod
     def d(tag, msg):
-        sys.stdout.write(fg.green+"DEBUG/"+str(tag)+"\t\t"+str(msg)+"\n")
+        sys.stdout.write("DEBUG/"+str(tag)+"\t\t"+str(msg)+"\n")
+
